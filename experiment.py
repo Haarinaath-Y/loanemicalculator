0d0988f535f630f83caefb1dcf0defebcb65457d
Nameless Code <harishy.bio@gmail.com>
2024-10-28 11:24:58 +0530

    icon

--- experiment.py
+++ experiment.py
@@ -34,7 +34,7 @@ for i, payment in enumerate(st.session_state.extra_payments):
 
     # Add empty space to push the delete button to the bottom
     with col3:
-        st.empty()  # Adds space at the top of the column
+        st.write("")  # Adds space at the top of the column
         if st.button(":material/delete:", key=f"remove_{i}"):
             remove_payment_row(i)
             st.rerun()  # Rerun to refresh the UI after deletion
